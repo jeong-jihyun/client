eb99e66b849018c8918fba87376ff00086374d95
jihyun.jeong <mujuk73@hotmail.com>
2023-08-09 23:14:08 +0900

    소스 커밋

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { HomePage } from "./pages/HomePage";
+import { Provider as ReduxProvider } from "react-redux";
+import {DndProvider} from 'react-dnd'
+import { BrowserRouter } from "react-router-dom";
+import { HTML5Backend } from "react-dnd-html5-backend";
+//import {AuthProvider} from './contexts'
+import { useStore } from "./store";
+import PageRouters from "./routes/PageRoutes";
 
 function App() {
+  const store = useStore();
+
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      </BrowserRouter>
+      <ReduxProvider store={store}>
+        <DndProvider backend={HTML5Backend}>
+          <BrowserRouter>
+            <PageRouters/>
+          </BrowserRouter>
+        </DndProvider>
+      </ReduxProvider>
     </>
   );
 }
