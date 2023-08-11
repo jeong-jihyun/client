a6cfa21e86d57aa1b64f807aaecb4566f242bec4
jihyun.Jeong <mujuk73@gmail.com>
2023-08-11 19:42:28 +0900

    Update PageRoutes.tsx
    라우터 정의

--- src/routes/PageRoutes.tsx
+++ src/routes/PageRoutes.tsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import { HomePage } from "../pages/HomePage";
 
 export default function PageRoutes() {
+    // 우선 메인 패이지만 붙여 작업을 진행 한다
+    // 추후에, 화면이 필요시 추가 하도록 한다
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
